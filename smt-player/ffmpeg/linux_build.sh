4ce83d0b78052654882ed0be0b83d869d57ba781
nobody <nobody@anybody.com>
2016-03-22 18:12:52 +0800

    add additional link options

--- smt-player/ffmpeg/linux_build.sh
+++ smt-player/ffmpeg/linux_build.sh
@@ -1,1 +1,7 @@
-./configure --enable-debug --enable-libx264 --enable-libx265 --enable-gpl --enable-nonfree
+function build
+{
+./configure --prefix=$PREFIX --enable-shared --disable-static --enable-debug --enable-libx264 --enable-libx265 --enable-gpl --enable-nonfree --extra-ldflags=-Wl,-rpath=$PREFIX/lib
+}
+
+PREFIX=$(pwd | xargs dirname)/gen
+build
