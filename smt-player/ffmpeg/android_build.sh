be3a32b947a4f48b2375d3905402f91bbe5efdcc
nobody <nobody@anybody.com>
2016-05-03 14:28:41 +0800

    change directory

--- smt-player/ffmpeg/android_build.sh
+++ smt-player/ffmpeg/android_build.sh
@@ -3,7 +3,8 @@ SYSTEMROOT=$NDK_HOME/platforms/android-24/arch-arm/
 TOOLCHAIN=$NDK_HOME/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64  
   
 function build_one  
-{  
+{ 
+mkdir -p $PREFIX 
 ./configure \
     --prefix=$PREFIX \
     --enable-shared \
@@ -20,7 +21,7 @@ function build_one
     $ADDITIONAL_CONFIGURE_FLAG
 }  
 CPU=arm  
-PREFIX=$(pwd | xargs dirname)/jni/gen
+PREFIX=$(pwd | xargs dirname)/android/jni/gen
 ADDI_CFLAGS="-marm -DSMT_ANDROID"  
 build_one  
 
--- smt-player/ffmpeg/linux_build.sh
+++ smt-player/ffmpeg/linux_build.sh
@@ -1,5 +1,6 @@
 function build
 {
+mkdir -p $PREFIX
 ./configure --prefix=$PREFIX --enable-shared --enable-static --enable-debug --enable-libx264 --enable-libx265 --enable-gpl --enable-nonfree --extra-ldflags=-Wl,-rpath=$PREFIX/lib
 make uninstall
 make clean
@@ -7,6 +8,6 @@ make
 make install
 }
 
-PREFIX=$(pwd | xargs dirname)/jni/gen
+PREFIX=$(pwd | xargs dirname)/gen
 mkdir -p $PREFIX
 build
